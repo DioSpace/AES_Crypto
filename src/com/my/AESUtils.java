b8046b9a676b145a1d587ec182ea1df34bd4eb15
Dio <zhendong2011@live.cn>
2020-10-07 20:14:56 +0800

    update

---
+++ src/com/my/AESUtils.java
@@ -0,0 +1,91 @@
+package com.my;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class AESUtils {
+    // 密匙
+    private static final String KEY = "thisisatestkey==";
+    // 偏移量
+    private static final String OFFSET = "1234567890abcdef";
+    // 编码
+    private static final String ENCODING = "UTF-8";
+    //算法
+    private static final String ALGORITHM = "AES";
+    // 默认的加密算法
+    private static final String CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding";
+
+    public static String encrypt(String data) {
+        try {
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            SecretKeySpec skeySpec = new SecretKeySpec(KEY.getBytes("ASCII"), ALGORITHM);
+            IvParameterSpec iv = new IvParameterSpec(OFFSET.getBytes());//使用CBC模式，需要一个向量iv，可增加加密算法的强度
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+            byte[] encrypted = cipher.doFinal(data.getBytes(ENCODING));
+            return new BASE64Encoder().encode(encrypted);//此处使用BASE64做转码
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String data) {
+        try {
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            SecretKeySpec skeySpec = new SecretKeySpec(KEY.getBytes("ASCII"), ALGORITHM);
+            IvParameterSpec iv = new IvParameterSpec(OFFSET.getBytes());//使用CBC模式，需要一个向量iv，可增加加密算法的强度
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] buffer = new BASE64Decoder().decodeBuffer(data);
+            byte[] encrypted = cipher.doFinal(buffer);
+            return new String(encrypted, ENCODING);//此处使用BASE64做转码
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String plaintext = "1234";
+        String ciphertext = encrypt(plaintext);
+        System.out.println("ciphertext:\n" + ciphertext);
+        String plaintext2 = decrypt(ciphertext);
+        System.out.println("plaintext2:\n" + plaintext2);
+    }
+}
--- src/com/my/MyUtil.java
+++ src/com/my/MyUtil.java
@@ -11,6 +11,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import sun.misc.BASE64Decoder;
@@ -21,7 +22,10 @@ import sun.misc.BASE64Encoder;
  */
 public class MyUtil {
 
+    // 加密密钥key
     private static String key = "thisisatestkey==";
+    // 偏移写死
+    private static String iv = "1234567890abcdef";
 
     static {
         System.out.println("key ：" + key + "\n====================以上是加密的key====================");
@@ -42,7 +46,9 @@ public class MyUtil {
             KeyGenerator keygen = KeyGenerator.getInstance("AES");
             //2.根据ecnodeRules规则初始化密钥生成器
             //生成一个128位的随机源,根据传入的字节数组
-            keygen.init(128, new SecureRandom(key.getBytes()));
+            //AES算法要求有一个可信任的随机数源，可以通过 SecureRandom类,内置两种随机数算法，NativePRNG和SHA1PRNG
+            SecureRandom random = new SecureRandom(key.getBytes()); //可替代 偏移ivi
+            keygen.init(128, random);
             //3.产生原始对称密钥
             SecretKey original_key = keygen.generateKey();
             //4.获得原始对称密钥的字节数组
@@ -50,15 +56,15 @@ public class MyUtil {
             //5.根据字节数组生成AES密钥
             SecretKey key = new SecretKeySpec(raw, "AES");
             //6.根据指定算法AES自成密码器
-            Cipher cipher = Cipher.getInstance("AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密(Decrypt_mode)操作，第二个参数为使用的KEY
             cipher.init(Cipher.ENCRYPT_MODE, key);
             //8.获取加密内容的字节数组(这里要设置为utf-8)不然内容中如果有中文和英文混合中文就会解密为乱码
             byte[] byte_encode = plaintext.getBytes("utf-8");
             //9.根据密码器的初始化方式--加密：将数据加密
             byte[] byte_AES = cipher.doFinal(byte_encode);
-            //10.将加密后的数据 作Base64加密成字符串
-            String AES_encode = new String(new BASE64Encoder().encode(byte_AES));
+            //10.将加密后的数据 依照Base64映射成字符串
+            String AES_encode = new BASE64Encoder().encode(byte_AES);
             //11.将字符串返回
             return AES_encode;
         } catch (NoSuchAlgorithmException e) {
@@ -91,7 +97,9 @@ public class MyUtil {
             KeyGenerator keygen = KeyGenerator.getInstance("AES");
             //2.根据ecnodeRules规则初始化密钥生成器
             //生成一个128位的随机源,根据传入的字节数组
-            keygen.init(128, new SecureRandom(key.getBytes()));
+            //AES算法要求有一个可信任的随机数源，可以通过 SecureRandom类,内置两种随机数算法，NativePRNG和SHA1PRNG
+            SecureRandom random = new SecureRandom(key.getBytes()); //可替代 偏移ivi
+            keygen.init(128, random);
             //3.产生原始对称密钥
             SecretKey original_key = keygen.generateKey();
             //4.获得原始对称密钥的字节数组
@@ -99,7 +107,7 @@ public class MyUtil {
             //5.根据字节数组生成AES密钥
             SecretKey key = new SecretKeySpec(raw, "AES");
             //6.根据指定算法AES自成密码器
-            Cipher cipher = Cipher.getInstance("AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密(Decrypt_mode)操作，第二个参数为使用的KEY
             cipher.init(Cipher.DECRYPT_MODE, key);
             //8.将加密并编码后的内容解码成字节数组
